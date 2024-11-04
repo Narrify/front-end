23bda4edea1e727ccdae9e1019b39c61b18b79b5
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2024-11-04 16:12:01 -0500

    Done Register & LogIn

---
+++ src/pages/log-in.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LogInComponent from "../components/LogIn";
+import AppTheme from "../components/shared/AppTheme";
+
+function LogIn() {
+	return (
+		<div>
+			<AppTheme>
+				<LogInComponent/>
+			</AppTheme>
+		</div>
+	);
+}
+
+export default LogIn;
---
+++ src/pages/register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegisterComponent from "../components/Register";
+import AppTheme from "../components/shared/AppTheme";
+
+function Register() {
+	return (
+		<div>
+			<AppTheme>
+				<RegisterComponent/>
+			</AppTheme>
+		</div>
+	);
+}
+
+export default Register;
